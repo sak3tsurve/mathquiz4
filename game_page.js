07ba7b3d545ddbacf65dd20a9387a15943bb4e14
saket54 <89304756+sak3tsurve@users.noreply.github.com>
2023-05-14 17:20:12 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,11 @@
+function addUser() {
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+   
+    localStorage.setItem("player2_name",player2_name);
+    localStorage.setItem("player1_name",player1_name);
+    
+    window.location.replace("game_page.html")
+}
+
+    
